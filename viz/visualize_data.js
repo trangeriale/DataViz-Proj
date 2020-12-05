c2130f2bc0fbf97a1d8d7ce6b9635fcfba87af2d
trangrei <trang.lt157@gmail.com>
2020-12-04 23:32:35 -0800

    add visualization

--- viz/visualize_data.js
+++ viz/visualize_data.js
@@ -14,47 +14,14 @@ var svg = d3.select('#my_dataviz')
         "translate(" + margin.left + "," + margin.top + ")");
 
 
-d3.csv("../cpugpu.csv", function(data)) {
-	var color = d3.scaleOrdinal()
-    .domain(["setosa", "versicolor", "virginica" ])
-    .range([ "#440154ff", "#21908dff", "#fde725ff"])
+d3.csv("../cpugpu.csv", function(data) {
+	//more like the scale of each
+	svg.selectAll()
+		.data(data, function(d) {return d.CPU + ':' + d.GPU;})
+		
 
-	dimensions = ["Game Rating", "Desktop Rating", "Work Rating"]
-
-	var y = {}
-
-	for (i in dimensions) {
-		name = dimensions[i]
-		y[name] = d3.scaleLinear()
-			.domain([0,350])
-			.range([height, 0])
-	}
-
-	x = d3.scalePoint()
-		.range([0, width])
-		.domain(dimensions)
-
-	var highlight = function(d) {
-		selected_rating = d.CPU
-
-		d3.selectAll(".line")
-	      .transition().duration(200)
-	      .style("stroke", "lightgrey")
-	      .style("opacity", "0.2")
-	    // Second the hovered specie takes its color
-	    d3.selectAll("." + selected_specie)
-	      .transition().duration(200)
-	      .style("stroke", color(selected_specie))
-	      .style("opacity", "1")
-	}
-
-	var doNotHighlight = function(d){
-	    d3.selectAll(".line")
-	      .transition().duration(200).delay(1000)
-	      .style("stroke", function(d){ return( color(d.CPU))} )
-	      .style("opacity", "1")
-  	}
-}
+	
+})
 
 
 

b7c8f8c44ce70483cf961dd752f3248dceb804d1
Trang Le <trang.lt157@gmail.com>
2022-12-06 19:58:40 -0800

    add zoom legend

--- docs/mainViz.js
+++ docs/mainViz.js
@@ -1,5 +1,7 @@
 //const { filter } = require("d3-array")
 
+//const { svg } = require("d3")
+
 Promise.all([
     d3.csv("https://raw.githubusercontent.com/trangeriale/DataViz-Proj/getPriceData/cpugpu.csv"),
     d3.csv("https://raw.githubusercontent.com/trangeriale/DataViz-Proj/getPriceData/cpu_price.csv"),
@@ -26,12 +28,11 @@ function vizData(ratingData, priceCPU, priceGPU) {
     how do I query the brand and color code
     case switch? oh or filter func js
     */
-
     const CPUList = ["AMD Ryzen", "AMD", "Intel"]
     const GPUList = ["AMD", "Intel", "Nvidia"]
     const CPUScale = d3.scaleOrdinal()
         .domain(CPUList)
-        .range(["#C4663A", "red", "#2B66AF"])
+        .range(["#C4663A", "black", "#015FF5"])
     const GPUScale = d3.scaleOrdinal()
         .domain(GPUList)
         .range(["#FFFFFF", "#2C66AF", "#76B900"])
@@ -49,9 +50,6 @@ function vizData(ratingData, priceCPU, priceGPU) {
         let priceStr = priceGPU[price].eBayAveragePriceNovember
         priceGPUObj[priceGPU[price].GPU] = parseInt(priceStr.slice(1).replace(',', ''))
     }
-
-    //console.log(priceGPUObj)
-
     const priceCPUObj = {}
     for (let price in priceCPU) {
         //console.log(priceCPU[price])
@@ -62,37 +60,16 @@ function vizData(ratingData, priceCPU, priceGPU) {
         //onsole.log(priceStr)
         priceCPUObj[priceCPU[price].CPU] = parseInt(priceStr.slice(1).replace(',', ''))
     }
+    const rescaleFactor = 15;
+    const m = 100; //margin
+    let height = Math.max(document.documentElement.clientHeight * 95 / 100 - m || 0, window.innerHeight * 95 / 100 - m || 0);
+    let width = Math.max(document.documentElement.clientWidth * 95 / 100 - m || 0, window.innerWidth * 95 / 100 - m || 0)
 
-    //console.log(priceCPUObj)
-    let height = 1000
-    const m = 100
-    let width = Math.max(document.documentElement.clientWidth - m || 0, window.innerWidth - m || 0)
-
-    const controllerH = 0
-    const controllerW = 600
-
-    //convert rating data from % string to int and find max and min
-
-    // const workPerfScale = d3.scaleLinear().domain(workPerformance)
-    //     .range([height - m, m])
-    //     .nice()
-
-    // const priceG = d3.extent(priceGPU, d => parseInt(d.eBayAveragePriceNovember.slice(1).replace(',', '')))
-    // //console.log(priceG)
-
-    // const priceC = d3.extent(priceCPU, d => parseInt(d.Price.slice(1)))
-    // //console.log(priceC)
-
-    // const priceBuild = [priceG[0] + priceC[0], priceG[1] + priceC[1]]
-    // //console.log(priceBuild)
-    // //const priceScale = d3.scaleLinear().domain(priceBuild).range([m / 4, width - 5 * m / 4]).nice()
     ratingData.forEach(d => {
         if (priceGPUObj[d.GPU] && priceCPUObj[d.CPU]) {
-            d.GPUPrice = priceGPUObj[d.GPU]
-            //console.log("GPU Price")
+            d.GPUPrice = priceGPUObj[d.GPU];
 
-            d.CPUPrice = priceCPUObj[d.CPU]
-            //console.log(d.CPUPrice)
+            d.CPUPrice = priceCPUObj[d.CPU];
         }
     })
 
@@ -101,65 +78,75 @@ function vizData(ratingData, priceCPU, priceGPU) {
             return processRating(d.workRating)
         }
     })
-    //const meanWorkPerf = d3.quantile
-    const workRatingArray = ratingData.map((d) => processRating(d.workRating))
-    //console.log(workRatingArray)
 
-    const percentile90 = d3.quantile(workRatingArray, 0.95)
-    //console.log(percentile90)
-
-    // const workPerfScale = d3.scaleLinear()
-    //     .domain([d3.quantile(workRatingArray, 0),
-    //     d3.quantile(workRatingArray, 0.95),
-    //     d3.quantile(workRatingArray, 1)])
-    //     .range([height - m, height / 5, m])
-    //     .nice()
     const workPerfScale = d3.scaleLinear().domain(workPerformance)
         .range([height - m, m])
 
-    console.log("work", workPerformance)
+    //  console.log("work", workPerformance)
 
     const priceDomain = d3.extent(ratingData, d => d.CPUPrice + d.GPUPrice)
-    console.log("price", priceDomain)
+    //   console.log("price", priceDomain)
 
     const priceScale = d3.scaleLinear().domain(priceDomain).range([m / 4, width - 5 * m / 4]).nice()
 
-    const dashBoard = d3.select("body")
+    const dashBoard = d3.select("body").select("#main-viz")
         .append("svg")
         .attr("class", "pcviz")
         .attr("viewBox", `0 0 ${width} ${height}`)
         .attr("width", width)
         .attr("height", height)
 
+    dashBoard.append("defs").append("SVG:clipPath")
+        .attr("id", "clip")
+        .append("SVG:rect")
+        .attr("width", width - m)
+        .attr("height", height - m * 3 / 2)
+        .attr("x", m)
+        .attr("y", m / 2)
+
+    const dataField = dashBoard.append('g').attr('clip-path', "url('#clip')");
+
+    /*
+    append axes label
+    */
     dashBoard.append("text")
         .attr("text-anchor", "middle")
         .attr("x", m)
         .attr("y", m / 2)
         .html("PC Rating")
-        .style("font-size", "1em")
-        .style("font-weight", "800")
-        .append("text")
-        .attr("text-anchor", "middle")
-        .attr("x", width - m)
-        .attr("y", height - m * 1.5)
-        .html("Build Price")
-        .style("font-size", "1em")
+        .attr("tabindex", "0")
+        .style("font-size", "1rem")
         .style("font-weight", "800")
 
-    dashBoard.append("g")
+    dashBoard.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", width - m)
+        .attr("y", height - m / 2)
+        .html("Build Price")
+        .style("font-size", "1rem")
+        .style("font-weight", "800")
+
+    const yAxisInit = d3.axisLeft(workPerfScale)
+        .tickSize(10)
+        .ticks(15, "+f")
+        .tickFormat(d => d);
+
+    const yAxis = dashBoard.append("g")
         .attr("id", "y-axis")
         .attr("transform", `translate(${m},${0})`)
-        .call(d3.axisLeft(workPerfScale)
-            .tickSize(5)
-            .ticks(15, "+f")
-            .tickFormat(d => d))
-        .append("g")
-        .attr("id", "x-axis")
-        .attr("transform", `translate(${0},${height - m})`)
-        .call(d3.axisBottom(priceScale)
-            .tickSize(5)
+        .style("font", "1rem Roboto Mono")
+        .call(yAxisInit)
+
+    const xAxisInit =
+        d3.axisBottom(priceScale)
+            .tickSize(10)
             .ticks(width / 50)
-            .tickFormat(d => d))
+            .tickFormat(d => d);
+
+    const xAxis = dashBoard.append("g").attr("id", "x-axis")
+        .attr("transform", `translate(${m},${height - m})`)
+        .style("font", "0.75rem Roboto Mono")
+        .call(xAxisInit);
 
     window.addEventListener('resize', () => {
         //width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
@@ -173,17 +160,62 @@ function vizData(ratingData, priceCPU, priceGPU) {
                 .ticks(width / 50))
     })
 
+    /*     const popOverDataPoint = (eventDataPoint) => {
+            console.log(eventDataPoint, "cliciked on");
+            const clickedId = `info-${eventDataPoint.srcElement.id}`
+            const newDataClicked = dashBoard
+                .append("rect")
+                .attr("position", "absolute")
+                .attr("class", "open")
+                .attr("id", clickedId)
+                .attr("transform", `translate(${eventDataPoint.x}, ${eventDataPoint.y - m})`)
+                .attr("width", "200")
+                .attr("height", "200")
+            //.style("fill", "red")
+    
+            compNum += 1
+            console.log("click dat", clickedData);
+            //console.log(clickedData[0].classList.contains("open"), "open?")
+    
+            document.addEventListener('click', (event) => {
+                let toBeHidden = document.getElementById(clickedId)
+                if (event.target.id !== eventDataPoint.srcElement.id || (clickedData.id === `info-${eventDataPoint.id}` && newDataClicked.classList.contains("open"))) {
+                    console.log("wahoo");
+                    console.log(toBeHidden, "hide")
+                    toBeHidden.classList.toggle("open")
+                    toBeHidden.setAttribute("style", "display: none")
+                } else {
+                    toBeHidden.setAttribute("style", "display: inline-block")
+                    toBeHidden.classList.toggle("open")
+                }
+            })
+    
+            clickedData = newDataClicked;
+        } */
+    /*  let compareList = []; */
+
+    /*     const clickDataPoint = (dataPoint) => {
+            const dataPointElem = document.getElementById(dataPoint.srcElement.id)
+    
+            compareList.push(dataPointElem)
+    
+            //add dotted circle outside based on higher rating here
+    
+            if (compareList.length > 3) {
+                compareList.shift();
+            }
+        } */
+
     let compNum = 0
 
     let dataDetail
 
     const clickDataPoint = (dataPoint) => {
         if (compNum >= 3) {
-            //console.log("here")
             d3.select(".dataToCompare:first-of-type").remove()
         }
 
-        dataDetail = d3.select("#dataDetail")
+        dataDetail = d3.select("#data-detail")
             .append("div")
             .attr("class", "dataToCompare")
             .style("padding", "1.5em 1em")
@@ -200,18 +232,7 @@ function vizData(ratingData, priceCPU, priceGPU) {
             + priceGPUObj[dataPoint.srcElement.__data__.GPU]} <br>
             Work rating: ${dataPoint.srcElement.__data__.workRating} `)
         compNum += 1
-        //.style("background-color", "white")
     }
-    //now draw the arc hurah
-    /*
-    what I need for the arc:
-    M workRating - radius, price A cpuRating, cpuRating (as the radius)
-        rotation(?still dont get why it didn't work) 0 0 0 workRating + 2*radius (the end point)
-    1. the position to start drawing
-
-    2. need rating for the arc radius.
-    position: 
-    */
     const gap = 2;
     let buildPrice;
     let radiusLeft;
@@ -219,23 +240,20 @@ function vizData(ratingData, priceCPU, priceGPU) {
     let path;
     let y;
 
-
-    let gpuData = d3.select("svg.pcviz").append("g").attr("id", "gpuViz")
+    dataField.append("g").attr("id", "gpuViz")
         .selectAll("path")
         .data(ratingData)
         .enter()
+        .append("g")
         .append("path")
         .on("click", (d) => console.log(d))
-        .attr("class", "datapoint")
+        .attr("id", (d) => `id-${d.Index}`)
+        .attr("class", "datapoint gpuData")
         .attr("d", (d) => {
             if (d.GPUPrice && d.CPUPrice) {
                 buildPrice = priceScale(d.GPUPrice + d.CPUPrice)
-                //console.log(pGPU)
-                radiusLeft = (processRating(d.GPURating)) / 15
-
-                //console.log("radius", rea)
+                radiusLeft = (processRating(d.GPURating)) / rescaleFactor
                 y = workPerfScale(processRating(d.workRating))
-                //console.log("y", y)
                 path = `M ${buildPrice + m} ${y - radiusLeft} A ${radiusLeft} ${radiusLeft} 0 0 0 ${buildPrice + m} ${y + radiusLeft}`
                 return path
             }
@@ -244,22 +262,20 @@ function vizData(ratingData, priceCPU, priceGPU) {
         .attr("stroke", "black")
         .on("click", clickDataPoint)
 
-    const cpu = d3.select("svg.pcviz").append("g").attr("id", "cpuViz")
-
-    let cpuData = cpu.selectAll("path")
+    dataField.append("g").attr("id", "cpuViz")
+        .selectAll("path")
         .data(ratingData)
         .enter()
         .append("path")
-        .on("click", (d) => console.log(d))
-        .attr("class", "datapoint")
+        .attr("id", (d) => `id-${d.Index}`)
+        .on("click", (d) => console.log("d is", d))
+        .attr("class", "datapoint cpuData")
         .attr("d", (d) => {
             if (d.GPUPrice && d.CPUPrice) {
                 buildPrice = priceScale(d.GPUPrice + d.CPUPrice)
-                radiusRight = (processRating(d.CPURating)) / 10
+                radiusRight = (processRating(d.CPURating)) / rescaleFactor
                 y = workPerfScale(processRating(d.workRating))
-                //console.log(y)
                 path = `M ${buildPrice + m + gap} ${y - radiusRight} A ${radiusRight} ${radiusRight} 180 0 1 ${buildPrice + m + gap} ${y + radiusRight}`
-                //path = "M 50,0 A 25 25 0 0 0 50 50"
                 return path
             }
         })
@@ -267,93 +283,58 @@ function vizData(ratingData, priceCPU, priceGPU) {
         .attr("stroke", "black")
         .on("click", clickDataPoint)
 
-    const limitPrice = document.querySelector("#limitPrice")
+    const view = dashBoard.append("rect").attr("class", "view")
+        .attr("fill", "none")
+        .attr("x", `${m / 2}`)
+        .attr("width", `${width - m / 2}`)
+        .attr("height", `${height}`);
 
-    let customizePriceDomain
-    let customPriceScale
-    let numFiltered
-    //add new price as new domain
-    limitPrice.addEventListener("change", (e) => {
-        const priceLimit = parseFloat(e.target.value)
-        customizePriceDomain = [priceDomain[0], priceLimit]
-        console.log("customize", customizePriceDomain)
-        //priceDomain = customizePriceDomain
-        customPriceScale = d3.scaleLinear().domain(customizePriceDomain).range([m / 4, width - 5 * m / 4]).nice()
-        //console.log("customize scale", customPriceScale(442))
+    function zoomed({ transform }) {
+        //console.log(transform, "transform")
+        view.attr("transform", transform);
+        let newPriceScale = transform.rescaleX(priceScale);
+        let newWorkPerfScale = transform.rescaleY(workPerfScale);
 
-        dashBoard.select("g#x-axis")
-            .call(d3.axisBottom(customPriceScale)
-                .tickSize(5)
-                .ticks(width / 50)
-                .tickFormat(d => d))
+        // update axes with these new boundaries
+        xAxis.call(xAxisInit.scale(newPriceScale));
+        yAxis.call(yAxisInit.scale(newWorkPerfScale));
 
-        const filteredData = []
-
-        ratingData.forEach(d => {
-            if ((d.GPUPrice + d.CPUPrice) <= priceLimit) {
-                numFiltered += 1
-                filteredData.push(d)
-            }
-        })
-        console.log("fitler", filteredData)
-        gpuFiltered = d3.select("svg.pcviz")
-            .select("g#gpuViz")
-            .selectAll("path.datapoint")
-            .data(filteredData)
-
-        //if (numFiltered < ratingData.length) {
-        //  console.log("l", filteredData.length)
-        gpuFiltered.exit()
-            .remove()
-        //}
-
-        gpuFiltered.enter()
-            .append("path")
-            .attr("class", "datapoint")
-            .attr("d", (d) => {
-                //if (d.GPUPrice && d.CPUPrice) {
-                buildPrice = customPriceScale(d.GPUPrice + d.CPUPrice)
-                //console.log(pGPU)
-                radiusLeft = (processRating(d.GPURating)) / 15
-
-                //console.log("radius", rea)
-                y = workPerfScale(processRating(d.workRating))
-                //console.log("y", y)
-                path = `M ${buildPrice + m} ${y - radiusLeft} A ${radiusLeft} ${radiusLeft} 0 0 0 ${buildPrice + m} ${y + radiusLeft}`
-                return path
-                //}
-            })
-            .attr("fill", (d) => GPUScale(categorizeDevice(d.GPU, GPUList)))
-            .attr("stroke", "black")
-            .on("click", clickDataPoint)
-
-        cpuFiltered = d3.select("svg.pcviz")
-            .select("g#cpuViz")
-            .selectAll("path.datapoint")
-            .data(filteredData)
-
-        //if (numFiltered < filteredData.length) {
-        cpuFiltered.exit()
-            .remove()
-        //}
-
-        cpuFiltered.enter()
-            .append("path")
-            .on("click", (d) => console.log(d))
-            .attr("class", "datapoint")
+        // update circle position
+        dashBoard
+            .selectAll("path.datapoint.gpuData")
             .attr("d", (d) => {
                 if (d.GPUPrice && d.CPUPrice) {
-                    buildPrice = customPriceScale(d.GPUPrice + d.CPUPrice)
-                    radiusRight = (processRating(d.CPURating)) / 10
-                    y = workPerfScale(processRating(d.workRating))
-                    //console.log(y)
+                    buildPrice = newPriceScale(d.GPUPrice + d.CPUPrice)
+                    radiusLeft = (processRating(d.GPURating)) / rescaleFactor
+                    y = newWorkPerfScale(processRating(d.workRating))
+                    path = `M ${buildPrice + m} ${y - radiusLeft} A ${radiusLeft} ${radiusLeft} 0 0 0 ${buildPrice + m} ${y + radiusLeft}`
+                    return path
+                }
+            }
+            )
+
+        dashBoard.selectAll("path.datapoint.cpuData")
+            .attr("d", (d) => {
+                if (d.GPUPrice && d.CPUPrice) {
+                    buildPrice = newPriceScale(d.GPUPrice + d.CPUPrice)
+                    radiusRight = (processRating(d.CPURating)) / rescaleFactor
+                    y = newWorkPerfScale(processRating(d.workRating))
                     path = `M ${buildPrice + m + gap} ${y - radiusRight} A ${radiusRight} ${radiusRight} 180 0 1 ${buildPrice + m + gap} ${y + radiusRight}`
-                    //path = "M 50,0 A 25 25 0 0 0 50 50"
                     return path
                 }
             })
-            .attr("fill", (d) => CPUScale(categorizeDevice(d.CPU, CPUList)))
-            .attr("stroke", "black")
-            .on("click", clickDataPoint)
-    })
+    }
+
+    const zoom = d3.zoom()
+        .scaleExtent([.5, 20])  // This control how much you can unzoom (x0.5) and zoom (x20)
+        .extent([[0, 0], [width, height]])
+        .on("zoom", zoomed);
+
+    function reset() {
+        dashBoard.transition()
+            .duration(750)
+            .call(zoom.transform, d3.zoomIdentity);
+    }
+
+    Object.assign(dashBoard.call(zoom).node(), { reset });
 }
